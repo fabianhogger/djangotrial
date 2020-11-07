3a3b6a4493a06ff0920770e5128e02b4d5440904
Fabian <draizfabian@gmail.com>
2020-11-07 20:53:43 +0200

    changed database settings to postgress db

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'Travelstuffdb',
+        'USER': 'postgres',
+        'PASSWORD':'654321',
+        'HOST':'localhost'
     }
 }
 
