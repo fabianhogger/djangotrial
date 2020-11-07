1e69e00ee0cebc00dfafdb6e6e6d68ebebbebd90
Fabian <draizfabian@gmail.com>
2020-11-07 21:22:39 +0200

    made the connection betweeen db and django with command python manage.py makemigrations

--- webapp/travelstuff/models.py
+++ webapp/travelstuff/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Furniture:
-    name: str
-    img: str
-    desc: str
-    price: int
+class Furniture(models.Model):
+    name= models.CharField(max_length=100)
+    img= models.ImageField(upload_to='pics')
+    desc=  models.TextField()
+    price= models.IntegerField
+    offer= models.BooleanField(default=False)
--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travelstuff.apps.TravelstuffConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
