a2d4d357aa0be4acdf68389265e43ef29743b311
Fabian <draizfabian@gmail.com>
2020-11-09 20:25:59 +0200

    writing scrape function

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -8,4 +8,15 @@ def scrape():
     session.headers={"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
     url = "https://www.theonion.com/"
     content=session.get(url,verify=False).content
-    soup=BSoup(content)
+    soup=BSoup(content,"html.parser")
+    News=soup.find_all('div',{"class":"curation-module__item"})
+    for article in News:
+        main=article.find_all('a')[0]
+        link=main['href']
+        image_src=str(main.find('imgr')['srcset']).split(" ")[-4]
+        title=main['title']
+        new_headline=Headline()
+        new_headline.title=title
+        new_headline.url=link
+        new_headline.image=image_src
+        new_headline.save()
