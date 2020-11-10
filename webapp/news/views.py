bf5649d9b0745b6d01066839c1d64eea1bf8ae79
Fabian <draizfabian@gmail.com>
2020-11-10 15:43:26 +0200

    changed find_all to articles

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -9,10 +9,11 @@ def scrape(request):
     url = "https://www.theonion.com/"
     content=session.get(url,verify=False).content
     soup=BSoup(content,"html.parser")
-    News=soup.find_all('div',{"class":"curation-module__item"})
+    News=soup.find_all('article',{"class":"sc-1pw4fyi-7 gDJTEP js_post_item"})
     print('entered scrape')
     print(News)
     for article in News:
+        print('entered loop')
         main=article.find_all('a')[0]
         link=main['href']
         #image_src=str(main.find('imgr')['srcset']).split(" ")[-4]
@@ -22,7 +23,7 @@ def scrape(request):
         new_headline.url=link
         #new_headline.image=image_src
         new_headline.save()
-        return HttpResponse("OK")
+        return redirect('news')
 
 def news_list(request):
     headlines=Headline.objects.all()[::-1]
