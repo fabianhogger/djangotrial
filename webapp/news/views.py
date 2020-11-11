410a5b7cf8fcd7a814d97e2a527f92ed7cb6921f
Fabian <draizfabian@gmail.com>
2020-11-11 18:45:25 +0200

    made the right checks

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -14,28 +14,21 @@ def scrape(request):
     News=soup.find_all('article',{"class":"sc-1pw4fyi-7 gDJTEP js_post_item"})
     for article in News:
         main=article.find_all('a')[0]
-        #title=article.find_all('h4')[0]
-        #link=main['href']
+        title=article.find_all('h4')[0]
+        link=main['href']
         images=main.find('img')
         if images is not None:
             if images.has_attr('srcset'):
-                print(images)
-
-        """
-        if images is not None and main.find('img')['srcset'] is  not None:
-            print('entered if')
-            image_src=str(main.find('img')['srcset']).split("w,")[0]
-        else:
-            image_src =None
-        print('title: ' ,title.text)
-        print('link: ' ,link)
-        print('image_src: ' ,image_src)
-        titlet=str(title.text)
-        if link is not None and image_src is not None and title is not None:
-            new_headline=Headline(title=titlet,image=image_src,url=link)
-            new_headline.save()
-        """
-    return redirect('../')
+                #print(images)
+                image_src=str(main.find('img')['srcset']).split("w,")[0]
+                print('title: ' ,title.text)
+                print('link: ' ,link)
+                print('image_src: ' ,image_src)
+                titlet=str(title.text)
+                if link is not None and image_src is not None and title is not None:
+                    new_headline=Headline(title=titlet,image=image_src,url=link)
+                    new_headline.save()
+    return redirect('news')
 
 def news_list(request):
     print("news_list called")
