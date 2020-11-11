677e7062dc0f2a9454fb564a167401d49aa0cff8
Fabian <draizfabian@gmail.com>
2020-11-11 18:33:20 +0200

    splitting images

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -12,13 +12,21 @@ def scrape(request):
     content=session.get(url,verify=True).content
     soup=BSoup(content,"html.parser")
     News=soup.find_all('article',{"class":"sc-1pw4fyi-7 gDJTEP js_post_item"})
-    print('entered scrape')
     for article in News:
-        print('entered loop')
         main=article.find_all('a')[0]
-        title=article.find_all('h4')[0]
-        link=main['href']
-        image_src=str(main.find('img')['srcset']).split("w,")[0]
+        #title=article.find_all('h4')[0]
+        #link=main['href']
+        images=main.find('img')
+        if images is not None:
+            if images.has_attr('srcset'):
+                print(images)
+
+        """
+        if images is not None and main.find('img')['srcset'] is  not None:
+            print('entered if')
+            image_src=str(main.find('img')['srcset']).split("w,")[0]
+        else:
+            image_src =None
         print('title: ' ,title.text)
         print('link: ' ,link)
         print('image_src: ' ,image_src)
@@ -26,7 +34,8 @@ def scrape(request):
         if link is not None and image_src is not None and title is not None:
             new_headline=Headline(title=titlet,image=image_src,url=link)
             new_headline.save()
-        return redirect('../')
+        """
+    return redirect('../')
 
 def news_list(request):
     print("news_list called")
