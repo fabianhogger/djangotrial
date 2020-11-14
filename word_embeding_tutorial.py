131e3733f2b43275c296a9afa5bfe86ada6b1ef5
Fabian <draizfabian@gmail.com>
2020-11-14 18:36:31 +0200

    word embeding tutorial2

--- word_embeding_tutorial.py
+++ word_embeding_tutorial.py
@@ -5,7 +5,10 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import tensorflow_datasets  as tfds
 
-embedding_layer=layers.Embedding(1000,5)
-result=embedding_layer(tf.constant([1,2,3]))
-print(result.numpy())
-print(result.numpy().shape)
+#embedding_layer=layers.Embedding(1000,5)
+#result=embedding_layer(tf.constant([1,2,3]))
+#print(result.numpy())
+#print(result.numpy().shape)
+(train_data,test_data), info=tfds.load('imdb_reviews/subwords8k',split=(tfds.Split.TRAIN,tfds.Split.TEST),with_info=True,as_supervised=True)
+encoder=info.features['text'].encoder
+print(encoder.subwords[:20])
