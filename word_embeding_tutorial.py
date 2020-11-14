8e4743440bd30c04ee4110fbcf4bbbd7f40f5df2
Fabian <draizfabian@gmail.com>
2020-11-14 18:21:54 +0200

    word embeding tutorial

---
+++ word_embeding_tutorial.py
@@ -0,0 +1,11 @@
+import io
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow_datasets  as tfds
+
+embedding_layer=layers.Embedding(1000,5)
+result=embedding_layer(tf.constant([1,2,3]))
+print(result.numpy())
+print(result.numpy().shape)
