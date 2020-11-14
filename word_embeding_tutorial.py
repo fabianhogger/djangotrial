a70804a206e56c30416f34d43caf59bd8b46c9a3
Fabian <draizfabian@gmail.com>
2020-11-14 19:12:22 +0200

    word embeding tutorial3

--- word_embeding_tutorial.py
+++ word_embeding_tutorial.py
@@ -11,4 +11,15 @@ import tensorflow_datasets  as tfds
 #print(result.numpy().shape)
 (train_data,test_data), info=tfds.load('imdb_reviews/subwords8k',split=(tfds.Split.TRAIN,tfds.Split.TEST),with_info=True,as_supervised=True)
 encoder=info.features['text'].encoder
-print(encoder.subwords[:20])
+#print(encoder.subwords[:20])
+padded_shapes=([None],())#PADDING OUR DATA SO THAT THEY HAVE THE SAME SHAPE
+train_batches=train_data.shuffle(1000).padded_batch(10,padded_shapes=padded_shapes)
+test_batches=test_data.shuffle(1000).padded_batch(10,padded_shapes=padded_shapes)
+embedding_dim=16
+model=keras.Sequential([layers.Embedding(encoder.vocab_size,embedding_dim),layers.GlobalAveragePooling1D(),layers.Dense(1,acivation='sigmoid')])
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+history=model.fit(train_batches,epochs=10,validation_data=test_batches,validation_steps=20)
+history_dict=history.history
+acc=history_dict['accuracy']
+val_acc=history_dict['val_acc']
+epochs=range(1,len(acc)+1)
