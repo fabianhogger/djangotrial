df89b17332f3b2ab5ad9da8dafbf6c4a861b2031
Fabian <draizfabian@gmail.com>
2020-11-21 17:51:42 +0200

    remove punctuation2

--- twd_sentiment_analysis.py
+++ twd_sentiment_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import string
 data=pd.read_csv('imdb_labelled.tsv',header=None,delimiter='\t')
 data.columns=['Text','Label']
 #print(data.head())
@@ -9,4 +10,5 @@ def remove_punct(text):
     text_nopunct=''
     text_nopunct=re.sub('['+string.punctuation+']','',text)
     return text_nopunct
-data['Text_Clean']=data['Text'].apply(lambda x: text_nopunct(x))
+data['Text_Clean']=data['Text'].apply(lambda x: remove_punct(x))
+#print(data.Text_Clean)
