f25146c815e756d213ea3d5567a7da9bca0cda3a
Fabian <draizfabian@gmail.com>
2020-12-11 22:47:15 +0200

    added other solution

--- findallitems.py
+++ findallitems.py
@@ -3,17 +3,33 @@
 def find_items(itemlist,value):
     indexlist=[]
     for item in itemlist:
-        print("item, ",item)
         if isinstance(item,list):
-            print("isinstance")
             result=find_items(item,value)
             indexlist.append(result)
+        elif item==value:
+            indexlist.append(1)
         else:
-            if item==value:
-                indexlist.append(1)
-            else:
                 indexlist.append(0)
+    print(itemlist)
+    print(indexlist)
+    print("ENDS")
     return indexlist
 
 lista=[0,1,2,3,1,[0,2,1],4,5,1]
-find_items(lista,1)
+find_items(lista,2)
+
+
+def index_all(lista,item):
+    indices=list()
+    for i in range(len(lista)):
+        if lista[i]==item:
+            indices.append([i])
+        elif isinstance(lista[i],list):
+            for index in index_all(lista[i],item):
+                indices.append([i]+index)
+    print(lista)
+    print(indices)
+    return indices
+
+
+index_all(lista,2)
