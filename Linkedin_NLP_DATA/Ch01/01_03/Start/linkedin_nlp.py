60c74228918d16b115ef049a4e2026c1d025adfe
Fabian <draizfabian@gmail.com>
2020-12-11 23:09:07 +0200

    data exploration

--- Linkedin_NLP_DATA/Ch01/01_03/Start/linkedin_nlp.py
+++ Linkedin_NLP_DATA/Ch01/01_03/Start/linkedin_nlp.py
@@ -1,8 +1,22 @@
 from nltk.corpus import stopwords
-
+import pandas as pd
 #print(stopwords.words('english')[0:10])
 rawData=open("SMSSpamCollection.tsv").read()
-print(rawData[:500])
+#print(rawData[:500])
 
 parsed_data=rawData.replace("\t","\n").split("\n")
-print(parsed_data[0:10])    
+#print(parsed_data[0:10])
+Label_list=parsed_data[0::2]
+text_list=parsed_data[1::2]
+#print(len(Label_list))
+#print(len(text_list))
+
+df=pd.DataFrame({"label":Label_list[:-1],"body_list":text_list})
+print(df.head())
+"""
+BETTER WAY TO HAVE READ THE SMSSpamCollection.tsv USING PANDAS
+"""
+df=pd.read_csv("SMSSpamCollection.tsv",sep="\t",header=None)
+df.columns=['label','body_text']
+#what is the shape of the data
+print("input data has {} rows and {} columns ".format(len(df),len(df.columns)))
