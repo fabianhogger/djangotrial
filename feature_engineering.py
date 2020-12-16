e49310c33debd5637119130f6d034f2054fe61d8
Fabian <draizfabian@gmail.com>
2020-12-16 17:37:58 +0200

    created 2 features

--- feature_engineering.py
+++ feature_engineering.py
@@ -8,6 +8,16 @@ Power transformations(square ,square root, etc)
 Standarizing Data
 """
 import pandas as pd
+import string
 pd.set_option('display.max_colwidth',100)
 df=pd.read_csv("SMSSpamCollection.tsv",sep="\t",header=None)
 df.columns=['label','body_text']
+
+#Create A text message length feature
+df['body_len']=df["body_text"].apply(lambda x:len(x)-x.count(" "))
+#Create  feature that show the percentage of punctuation in text
+def count_punct(text):
+    count=sum([1 for char in text if char in string.punctuation])
+    return count/(len(text)-text.count(" "))
+df["punct_percent"]=df["body_text"].apply(lambda x:count_punct(x))
+print(df["punct_percent"][:20])
