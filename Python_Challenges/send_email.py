9e2d3dae7d28bfebe33c95da5eef4d13f83652eb
Fabian <draizfabian@gmail.com>
2020-12-19 19:56:55 +0200

    send mail though python

---
+++ Python_Challenges/send_email.py
@@ -0,0 +1,8 @@
+import smtplib
+def send_mail(to,subject,body):
+    gmail_user='draizfabian@gmail.com'
+    gmail_password='adasd'
+    sent_from = 'draizfabian@gmail.com'
+    to = [to]
+    subject = subject
+    body = body
