90642279ef19542147355ed5c8d46ba515893904
Fabian <draizfabian@gmail.com>
2020-12-20 17:39:03 +0200

    remove debug prints

--- Python_Challenges/monte_carlo_sim.py
+++ Python_Challenges/monte_carlo_sim.py
@@ -1,19 +1,16 @@
-import pandas
 import random
 def sim(*args):
-    max_value=sum(args)
     table=dict()
     dice_list=[arg for arg in args]
     for i in range(0,1000000):
         num_list=[(random.randint(1,dice)) for dice in dice_list]
         newval=sum(num_list)
-        #print("new sum ",newval)
-        #print(table.get(str(newval),1))
-        d={str(newval):table.get(str(newval),0)+1}
+        d={str(newval):(table.get(str(newval),0)+1)/1000000}
         #print(d)
         table.update(d)
 
     print(table)
+    return table
 val1=4
 val2=6
 val3=6
