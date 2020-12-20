132e0b89d17acd1f822699b95c6b0d58ba20f0f9
Fabian <draizfabian@gmail.com>
2020-12-20 17:47:58 +0200

    added tutorial solution

--- Python_Challenges/monte_carlo_sim.py
+++ Python_Challenges/monte_carlo_sim.py
@@ -6,7 +6,6 @@ def sim(*args):
         num_list=[(random.randint(1,dice)) for dice in dice_list]
         newval=sum(num_list)
         d={str(newval):(table.get(str(newval),0)+1)/1000000}
-        #print(d)
         table.update(d)
 
     print(table)
@@ -15,3 +14,13 @@ val1=4
 val2=6
 val3=6
 sim(val1,val2,val3)
+
+from Collections import Counter
+
+def roll_dice(*dice,num_trial=1_000_000):
+    counts=Counter()
+    for roll in range(num_trials):
+        counts=[sum((randint(1,sides) for sides in dice))]+=1
+    print('\n Outcome probability\n')
+    for outcome in range(len(dice,sum(dice)+1)):
+        print('{}\t{:0.2f}%'.format(outcome,counts[outcome]*100/num_trials))
