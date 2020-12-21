b877a4628a1656514aa5323bb5f8d044f8681e68
Fabian <draizfabian@gmail.com>
2020-12-21 19:55:23 +0200

    at rand time & greek letters

--- Python_Challenges/send_email.py
+++ Python_Challenges/send_email.py
@@ -1,56 +1,35 @@
 import smtplib
+import email.mime.text
+import time
+import random
 def send_email(to,subject,body):
-    gmail_user='draizfabian@gmail.com'
-    gmail_password='@pass'
-    sent_from = 'draizfabian@gmail.com'
-    to = [to]
-    subject = subject
-    body = body
 
-    email_text = """\
-    From: %s
-    To: %s
-    Subject: %s
+    for i in range(0,100):
+        gmail_user='your@gmail.com'
+        gmail_password='@password'
+        sent_from = 'your@gmail.com'
+        to = to
+        subject = subject
+        body =body
 
-    %s
-    """ % (sent_from, ", ".join(to), subject, body)
+        email_text =email.mime.text.MIMEText("""\
+        From: %s
+        To: %s
+        Subject: %s
 
-    server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server_ssl.ehlo()
-    print("Helo Successful") # optional
-    server_ssl.login(gmail_user, gmail_password)
-    print("Login Successful")
-    server_ssl.sendmail(sent_from, to, email_text)
-    server_ssl.close()
+        %s
+        """ % (sent_from, to, subject, body), _charset="UTF-8")
+        server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server_ssl.ehlo()
+        print("Helo Successful") # optional
+        server_ssl.login(gmail_user, gmail_password)
+        print("Login Successful")
+        server_ssl.sendmail(sent_from, to, email_text.as_string())
+        server_ssl.close()
+        sleep_val=random.randint(300,550)
+        time.sleep(sleep_val)
 
 to=input("Give receive adress :")
 subject=input("Give Subject :")
 body=input("Write text here :")
-print(type(to),type(subject),type(body  ))
 send_email(to,subject,body)
-"""
-import smtplib
-def send_mail(to,subject,body):
-    gmail_user='draizfabian@gmail.com'
-    gmail_password='adasd'
-    sent_from = 'draizfabian@gmail.com'
-    to = to
-    subject = subject
-    body = body
-
-
-    server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server_ssl.ehlo()
-    print("Helo Successful") # optional
-    server_ssl.login(gmail_user, gmail_password)
-    print("Login Successful")
-    server_ssl.sendmail(sent_from, to, email_text)
-    server_ssl.close()
-
-
-to=input("Give receive adress :")
-subject=input("Give Subject :")
-body=input("Write text here :")
-send_mail(to,subject,body)
-
-"""
