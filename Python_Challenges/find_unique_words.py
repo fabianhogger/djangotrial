3ec77deaa74d58f43ccd0348d8f35ded987bdb20
Fabian <draizfabian@gmail.com>
2020-12-21 19:28:39 +0200

    find unique words and return top 20 most common by count

--- Python_Challenges/find_unique_words.py
+++ Python_Challenges/find_unique_words.py
@@ -1,10 +1,23 @@
 import string
 import re
-import nltk
-import pandas as pd
+
+
+def clean_text(text):
+    #text=''.join([char for char in text if char not in string.punctuation])#removing punctuation
+    tokens=re.split('\W+',text)
+    #print(tokens)
+    #text=[word for word in tokens if word not in stopwords]
+    return tokens
+
 def count_unique(file_path):
-    df=pd.read_csv(file_path,sep=',',header=None)
-    df.columns(text)
-    text=''.join([char for char in df.text if char not in string.punctuation])
-    print(text)
-count_unique("SMSSpamCollection.tsv")
+    words=[]
+    with open(file_path,'r') as file:
+
+        for line in file:
+            a=re.split('\W+',line)
+            words+=a
+    word_count=[(words.count(word)) for word in words ]
+
+    print(words)
+    print(word_count)
+count_unique("file.txt")
