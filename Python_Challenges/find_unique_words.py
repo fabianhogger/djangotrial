cd5d848234c2219e88035b2bf011e1b417820c8f
Fabian <draizfabian@gmail.com>
2020-12-21 19:43:57 +0200

    done

--- Python_Challenges/find_unique_words.py
+++ Python_Challenges/find_unique_words.py
@@ -1,18 +1,14 @@
-import string
 import re
-
-
-
 
 def count_unique(file_path):
     words=[]
     with open(file_path,'r') as file:
         for line in file:
-            a=re.split('\W+',line)
+            a=re.split('\W+',line.lower())
             words+=a
     word_count=[(word,words.count(word)) for word in words ]
     res=sorted(word_count,key=lambda x:x[1],reverse=True)
-    return res
+    return res[:20]
 
 res=count_unique("file.txt")
 print(res)
