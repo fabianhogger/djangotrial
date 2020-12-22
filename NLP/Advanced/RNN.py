cce690dc56368b60873cc487ce96478861355925
Fabian <draizfabian@gmail.com>
2020-12-22 20:06:47 +0200

    model ready

--- NLP/Advanced/RNN.py
+++ NLP/Advanced/RNN.py
@@ -17,10 +17,40 @@ tokenizer=Tokenizer()
 tokenizer.fit_on_texts(X_train)#makes av vocabulary with an index number for every word
 
 #Use the tokenizer to tranform the train and test sets
-X_train_seq=tokenizer.text_to_sequences(X_train)#replaces the words with their indexes
-X_test_seq=tokenizer.text_to_sequences(X_test)
+X_train_seq=tokenizer.texts_to_sequences(X_train)#replaces the words with their indexes
+X_test_seq=tokenizer.texts_to_sequences(X_test)
 
 
 #Padding
 X_train_seq_padded=pad_sequences(X_train_seq,50)
 X_test_seq_padded=pad_sequences(X_test_seq,50)
+
+
+import keras.backend as K
+from keras.layers import Dense,Embedding,LSTM
+from keras.models import Sequential
+
+def recall_m(y_true,y_pred):
+    true_positives=K.sum(K.round(K.clip(y_true*y_pred,0,1)))
+    possible_positives=K.sum(K.round(K.clip(y_true,0,1)))
+    recall=true_positives/(possible_positives+K.epsilon())
+    return recall
+
+def precision_m(y_true,y_pred):
+    true_positives=K.sum(K.round(K.clip(y_true*y_pred,0,1)))
+    predicted_positives=K.sum(K.round(K.clip(y_pred,0,1)))
+    precision=true_positives/(predicted_positives+K.epsilon())
+    return precision
+
+#Create Model
+model=Sequential()
+model.add(Embedding(len(tokenizer.index_word)+1,32))
+model.add(LSTM(32,dropout=0,recurrent_dropout=0))
+model.add(Dense(32,activation='relu'))
+model.add(Dense(1,activation='softmax'))
+print(model.summary())
+
+model.compile(optimizer="adam",loss="binary_crossentropy",metrics=['accuracy',precision_m,recall_m])
+
+
+history=model.fit(X_train_seq_padded,y_train,batch_size=32,epochs=10,validation_data=(X_test_seq_padded,y_test))
