7936c05f892faaaf40fd0c521fc241d32234e421
Fabian <draizfabian@gmail.com>
2020-12-26 15:57:36 +0200

    save as csv

--- Python_Challenges/merge_csv.py
+++ Python_Challenges/merge_csv.py
@@ -1,11 +1,9 @@
 import pandas as pd
 
 
-def merge(file1,file2):
+def merge(file1,file2,name):
     df=pd.read_csv(file1)
-    print(df.head())
     df2=pd.read_csv(file2)
-    print(df2.head())
     f=df.merge(df2,how='outer')
-    print(f.head())
+    f.to_csv(path_or_buf=name)
 merge("file1.csv","file2.csv")
