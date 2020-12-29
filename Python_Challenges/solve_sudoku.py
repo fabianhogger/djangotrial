dc823fa92b443b5d989ee8797348c05f366693fd
Fabian <draizfabian@gmail.com>
2020-12-29 18:10:11 +0200

    done by looking for every box missing values and then possible places

--- Python_Challenges/solve_sudoku.py
+++ Python_Challenges/solve_sudoku.py
@@ -8,45 +8,68 @@ puzzle=[[5,3,0,0,7,0,0,0,0],
         [0,0,0,4,1,9,0,0,5],
         [0,0,0,0,8,0,0,7,9]]
 
+        [[5, 3, 4, 6, 7, 8, 9, 1, 2],
+         [6, 7, 2, 1, 9, 5, 3, 4, 8],
+         [1, 9, 8, 3, 4, 2, 5, 6, 7],
+         [8, 5, 9, 7, 6, 1, 4, 2, 3],
+         [4, 2, 6, 8, 5, 3, 7, 9, 1],
+         [7, 1, 3, 9, 2, 4, 8, 5, 6],
+         [9, 6, 1, 5, 3, 7, 2, 8, 4],
+         [2, 8, 7, 4, 1, 9, 6, 3, 5],
+         [3, 4, 5, 2, 8, 6, 1, 7, 9]]
+def check_line(puzzle,i,k):
+    if k not in puzzle[i]:
+        return True
+    else:
+        return False
+def check_column(puzzle,d,k):
+    column=[]
+    for line in puzzle:
+        column.append(line[d])
+    if k not in column:
+        return True
+    else:
+        return False
 
 def solve_sudoku(puzzle):
     i=0
     j=0
-    for m in range(0,3):
+    changed=True
+    while(changed==True):
+        changed=False
+        i=0
         j=0
-        for n in range(0,3):
-            print(j)
-            box=[puzzle[i][j],puzzle[i][j+1],puzzle[i][j+2],
-                puzzle[i+1][j],puzzle[i+1][j+1],puzzle[i+1][j+2],
-                puzzle[i+2][j],puzzle[i+2][j+1],puzzle[i+2][j+2]]
-            included=[number for number in box if number!=0]
-            print("included in box",included)
-            missing=[number for number in [1,2,3,4,5,6,7,8,9] if number not in included]
-            print("missing from box",missing)
+        for m in range(0,3):
+            j=0
+            for n in range(0,3):
+                print(j)
+                box=[puzzle[i][j],puzzle[i][j+1],puzzle[i][j+2],
+                    puzzle[i+1][j],puzzle[i+1][j+1],puzzle[i+1][j+2],
+                    puzzle[i+2][j],puzzle[i+2][j+1],puzzle[i+2][j+2]]
+                included=[number for number in box if number!=0]
+                print("included in box",included)
+                missing=[number for number in [1,2,3,4,5,6,7,8,9] if number not in included]
+                print("missing from box",missing)
 
-            for k in missing:
-                val=0
-                flag1=False
-                flag2=False
-                flag3=False
-                if  k not in puzzle[i]:
-                    val=val+1
-                    flag1=True
-                if k not in puzzle[i+1]:
-                    val=val+1
-                    flag2=True
-                if k not in puzzle[i+2]:
-                    val=val+1
-                    flag3=True
-                if val==1:
-                    if flag1==True:
-                        print("We can replace {} at line {}".format(k,i))
-                    elif flag2==True:
-                        print("We can replace {} at line {}".format(k,i+1))
-                    elif flag3==True:
-                        print("We can replace {} at line {}".format(k,i+2))
-            j=j+3
-        i=i+3
+                for k in missing:
+                    n_of_positions=0
 
+                    for row in range(i,i+3):
+                        for column in range(j,j+3):
+                            if puzzle[row][column]==0:
+                                res1=check_line(puzzle,row,k)
+                                res2=check_column(puzzle,column,k)
+                                if res1 and res2:
+                                    print("Available position for {} at row {} column {}".format(k,row,column))
+                                    n_of_positions=n_of_positions+1
+                                    pos=(row,column)
+                    print("DONE WITH {}, number of positions {}".format(k,n_of_positions))
+                    if n_of_positions==1:
+                        print("ONE POSITION FOR {}, AT {}".format(k,pos))
+                        puzzle[pos[0]][pos[1]]=k
+                        changed=True
+                j=j+3
+            i=i+3
+    print(puzzle)
 
 solve_sudoku(puzzle)
