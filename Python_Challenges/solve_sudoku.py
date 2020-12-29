98eb93f8f7474b70e5facfe5f6d6f705460a45a5
Fabian <draizfabian@gmail.com>
2020-12-29 18:22:18 +0200

    clean up more

--- Python_Challenges/solve_sudoku.py
+++ Python_Challenges/solve_sudoku.py
@@ -14,8 +14,6 @@ def check_column(puzzle,d,k):
         return False
 
 def solve_sudoku(puzzle):
-    i=0
-    j=0
     changed=True
     while(changed==True):
         changed=False
@@ -24,35 +22,33 @@ def solve_sudoku(puzzle):
         for m in range(0,3):
             j=0
             for n in range(0,3):
-                #print(j)
                 box=[puzzle[i][j],puzzle[i][j+1],puzzle[i][j+2],
                     puzzle[i+1][j],puzzle[i+1][j+1],puzzle[i+1][j+2],
                     puzzle[i+2][j],puzzle[i+2][j+1],puzzle[i+2][j+2]]
                 included=[number for number in box if number!=0]
-                #print("included in box",included)
                 missing=[number for number in [1,2,3,4,5,6,7,8,9] if number not in included]
-                #print("missing from box",missing)
-
                 for k in missing:
                     n_of_positions=0
-
                     for row in range(i,i+3):
                         for column in range(j,j+3):
                             if puzzle[row][column]==0:
                                 res1=check_line(puzzle,row,k)
                                 res2=check_column(puzzle,column,k)
                                 if res1 and res2:
-                                    #print("Available position for {} at row {} column {}".format(k,row,column))
                                     n_of_positions=n_of_positions+1
                                     pos=(row,column)
-                    #print("DONE WITH {}, number of positions {}".format(k,n_of_positions))
                     if n_of_positions==1:
-                        #print("ONE POSITION FOR {}, AT {}".format(k,pos))
                         puzzle[pos[0]][pos[1]]=k
                         changed=True
                 j=j+3
             i=i+3
     return puzzle
+
+from itertools import product
+
+
+
+
 
 puzzle=[[5,3,0,0,7,0,0,0,0],
         [6,0,0,1,9,5,0,0,0],
