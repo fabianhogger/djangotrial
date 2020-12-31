684c6374447659866a0395a9e89ebf64f48f4e9b
Fabian <draizfabian@gmail.com>
2020-12-31 12:42:27 +0200

    prepe doc2vec for ml

--- NLP/Advanced/doc2vec.py
+++ NLP/Advanced/doc2vec.py
@@ -9,3 +9,9 @@ messages=messages.drop(labels=['Unnamed: 2','Unnamed: 3','Unnamed: 4'],axis=1)
 messages.columns=['label','text']
 messages['text_clean']=messages['text'].apply(lambda x:gensim.utils.simple_preprocess(x))
 X_train,X_test,y_train,y_test=train_test_split(messages['text_clean'],messages['label'],test_size=0.2)
+
+#Doc2Vec requires the documents to be tagged
+tagged_docs=[ gensim.models.doc2vec.TaggedDocument(v,[i]) for v,i in enumerate(X_train)]
+#Training the model
+d2v_model=gensim.models.Doc2Vec(tagged_docs,vector_size=100,window=5,min_count=2)
+d2v_model.infer_vector(['i',' am',' learning ','nlp'])
