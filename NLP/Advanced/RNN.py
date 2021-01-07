3b99369439f7999f0396bf786d4f9ff6c7eb9ede
Fabian <draizfabian@gmail.com>
2021-01-07 19:12:54 +0200

    balance dataset to get better results

--- NLP/Advanced/RNN.py
+++ NLP/Advanced/RNN.py
@@ -2,9 +2,20 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 pd.set_option('display.max_colwidth',1000)
-messages=pd.read_csv("data/spam.csv",encoding='latin-1')
-messages=messages.drop(labels=["Unnamed: 2","Unnamed: 3","Unnamed: 4"],axis=1)
-messages.columns=['label','text']
+messages=pd.read_csv("data/spam.csv",encoding='latin-1',header=None)
+messages.columns=['label','text','useless','useless1','useless2',]
+print(messages['label'].value_counts())
+"""
+ham     4825
+spam     747
+"""
+spam_messages=messages[messages['label']=='spam']
+print("spam",spam_messages)
+messages=messages.append([spam_messages]*5,ignore_index=True)
+
+print("new df ",messages['label'].value_counts())
+
+
 labels=np.where(messages['label']=='spam',1,0)
 print(messages.head())
 X_train,X_test,y_train,y_test=train_test_split(messages['text'],labels,test_size=0.2)
@@ -19,11 +30,13 @@ tokenizer.fit_on_texts(X_train)#makes av vocabulary with an index number for eve
 #Use the tokenizer to tranform the train and test sets
 X_train_seq=tokenizer.texts_to_sequences(X_train)#replaces the words with their indexes
 X_test_seq=tokenizer.texts_to_sequences(X_test)
-
+print(X_train_seq[0])
 
 #Padding
-X_train_seq_padded=pad_sequences(X_train_seq,100)
-X_test_seq_padded=pad_sequences(X_test_seq,100)
+X_train_seq_padded=pad_sequences(X_train_seq,50)
+X_test_seq_padded=pad_sequences(X_test_seq,50)
+print(X_train_seq_padded[0])
+
 
 
 import keras.backend as K
@@ -37,7 +50,7 @@ def recall_m(y_true,y_pred):
     return recall
 
 def precision_m(y_true,y_pred):
-    true_positives=K.sum(K.round(K.clip(y_true*y_pred,0,1)))
+    true_positives=K.sum(K.round(K.clip(y_true * y_pred,0,1)))
     predicted_positives=K.sum(K.round(K.clip(y_pred,0,1)))
     precision=true_positives/(predicted_positives+K.epsilon())
     return precision
@@ -54,17 +67,18 @@ model.compile(optimizer="adam",loss="binary_crossentropy",metrics=['accuracy',pr
 
 
 history=model.fit(X_train_seq_padded,y_train,batch_size=32,epochs=10,validation_data=(X_test_seq_padded,y_test))
+
 """100 -input size loss: 13.2312 - accuracy: 0.1371 - precision_m: 0.1370 - recall_m: 0.9929 - val_loss: 13.3892 - val_accuracy: 0.1220 - val_precision_m: 0.1218 - val_recall_m: 0.9714
     50 input size  loss: 13.3413 - accuracy: 0.1299 - precision_m: 0.1298 - recall_m: 0.9714 - val_loss: 12.9516 - val_accuracy: 0.1507 - val_precision_m: 0.1507 - val_recall_m: 1.0000
 """
 
 import matplotlib.pyplot as plt
-for i in ['accuracy','accuracy','recall']:
+for i in ['accuracy','precision_m','recall_m']:
     acc=history.history[i]
     val_acc=history.history['val_{}'.format(i)]
     epochs=range(1,len(acc)+1)
     plt.figure()
-    plt.plot(epochs,acc,label='Training Accuracy')
-    plt.plot(epochs,val_acc,label='validation Accuracy')
+    plt.plot(epochs,acc,label='Training '+i)
+    plt.plot(epochs,val_acc,label='validation '+i)
     plt.legend()
     plt.show()
