cf3fc91b4a02978dc666ec23b593ce7a9255b304
Fabian <draizfabian@gmail.com>
2021-01-10 22:58:12 +0200

    fit Randomforest

--- NLP/Advanced/w2v_model.py
+++ NLP/Advanced/w2v_model.py
@@ -10,8 +10,8 @@ y_test=pd.read_csv('data/y_test.csv')
 #train word2vec model on X_train
 w2v_model=gensim.models.Word2Vec(X_train,size=100,window=5,min_count=2)
 words=set(w2v_model.wv.index2word)
-X_train_vect=np.array([np.array([w2v_model.wv[i] for i in ls if i in words] for ls in X_train['clean_text']])
-X_test_vect=np.array([np.array([w2v_model.wv[i] for i in ls if i in words] for ls in X_test['clean_text']])
+X_train_vect=np.array([np.array([w2v_model.wv[i] for i in ls if i in words]) for ls in X_train['clean_text']])
+X_test_vect=np.array([np.array([w2v_model.wv[i] for i in ls if i in words]) for ls in X_test['clean_text']])
 
 #vrisko to meso vector
 X_train_vect_avg=[]
@@ -37,4 +37,4 @@ y_pred=rf_model.predict(X_test_vect_avg)
 from sklearn.metrics import precision_score,recall_score
 precision=precision_score(y_test,y_pred)
 recall=recall_score(y_test,y_pred)
-print("Precision: {}, Recall: {}, Accuracy:{} ".format(round(precision,3),round(recall,3), round((y_pred==y_test['label'].sum()/len(y_pred)),3))
+print("Precision: {}, Recall: {}, Accuracy:{} ".format(round(precision,3),round(recall,3), round((y_pred==y_test['label'].sum()/len(y_pred)),3)))
