9fc959da3881e5fdd85128c264ba0c9b96980ef0
Fabian <draizfabian@gmail.com>
2021-01-12 00:06:25 +0200

    clean titanic data

--- MachineLearningFoundations/titanic_clean.py
+++ MachineLearningFoundations/titanic_clean.py
@@ -10,3 +10,8 @@ titanic.head()
 #create indcator for Cabin
 titanc['Cabin_ind']=np.where(titanic['Cabin'].isnull(), 0,1)
 #convert sex to numerical
+gender={'male':0,'female':1}
+titanic['Sex']=titanic['Sex'].map(gendere)
+
+#drop Cabin and Embarked
+titanic.drop(['Cabin','Enbarked'],axis=1,inplace=True)
