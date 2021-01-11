81c9ecff703c0e7efe5a143218bce99ce8f9500b
Fabian <draizfabian@gmail.com>
2021-01-11 23:09:17 +0200

    machine learning essentials

---
+++ MachineLearningFoundations/mlf1.py
@@ -0,0 +1,25 @@
+import pandas
+import numpy as np
+import seaborn as sn
+import matplotlib.pyplot as plt
+
+
+titanic=pd.read_csv("titanic.csv")
+cat_feat=['PassengerId','Name','Ticket','Sex','Cabin','Embarked']
+titanic.drop(cat_feat,axis=1,inplace=True)
+titanic.head()
+
+print(titanic.groupby('Survived').mean())
+
+print(titanic.groupby(titanic['Age'].isnull()).mean())
+for i in ['Age','Fare']:
+    died=list(titanic[titanic['Survived']==0][i].dropna())
+    survived=list(titanic[titanic['Survived']==1][i].dropna())
+    xmin=min(min(died),min(survived))
+    xmax=max(max(died),max(survived))
+    width=(xmax-xmin)/40
+    sns.displot(died,color='r',kde=False,bins=np.arange(xmin,xmax,width))
+    sns.displot(survived,color='g',kde=False,bins=np.arange(xmin,xmax,width))
+    plt.legend(['did not survive','Survived'])
+    plt.title('Overlaid histogram for {}'.format(i))
+    plt.show()
