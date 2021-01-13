9bfd86ac4838cc5b4857c76d3f70cf76c2b4f399
Fabian <draizfabian@gmail.com>
2021-01-13 18:00:11 +0200

    clean cotninous features

--- MachineLearningFoundations/basic_ml.py
+++ MachineLearningFoundations/basic_ml.py
@@ -4,3 +4,10 @@ import seaborn as sns
 
 titanic=pd.read_csv("titanic.csv")
 titanic.head()
+
+
+for i,col in enumerate(['SibSp','Parch']):
+    plt.figure(i)
+    sns.catplot(x=col,y='Survived',data=titanic,kind='point',aspect=2, )
+titanic['family_cnt']=titanic['SibSp']+titanic['Parch']
+titanic.to_csv("/filapath",index=False)
