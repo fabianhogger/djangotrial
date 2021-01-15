381bce95da4c653a51e8d5c29d793dcfcc9d4404
Fabian <draizfabian@gmail.com>
2021-01-15 21:48:21 +0200

    latent dirchlet distibution guide

---
+++ TM/guide.py
@@ -0,0 +1,8 @@
+doc1 = "Sugar is bad to consume. My sister likes to have sugar, but not my father."
+doc2 = "My father spends a lot of time driving my sister around to dance practice."
+doc3 = "Doctors suggest that driving may cause increased stress and blood pressure."
+doc4 = "Sometimes I feel pressure to perform well at school, but my father never seems to drive my sister to do better."
+doc5 = "Health experts say that Sugar is not good for your lifestyle."
+
+# compile documents
+doc_complete = [doc1, doc2, doc3, doc4, doc5]
