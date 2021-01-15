05cca15f02ae7720ac1bff384e321b8aa5ab5152
Fabian <draizfabian@gmail.com>
2021-01-15 22:55:19 +0200

    create matrices

--- TM/guide.py
+++ TM/guide.py
@@ -25,7 +25,17 @@ doc_clean = [clean(doc).split() for doc in doc_complete]
 
 import gensim
 from gensim import corpora
- #Creating the term dictionary of our courpus, where every unique term is assigned an index. dictionary = corpora.Dictionary(doc_clean)
-
+#Creating the term dictionary of our courpus, where every unique term is assigned an index.
+ dictionary = corpora.Dictionary(doc_clean)
 # Converting list of documents (corpus) into Document Term Matrix using dictionary prepared above.
 doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_clean]
+# Creating the object for LDA model using gensim library
+Lda = gensim.models.ldamodel.LdaModel
+
+# Running and Trainign LDA model on the document term matrix.
+ldamodel = Lda(doc_term_matrix, num_topics=3, id2word = dictionary, passes=50)
+
+print(ldamodel.print_topics(num_topics=3, num_words=3))
+"""
+
+"""
