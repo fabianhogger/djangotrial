7ad26bbf51711bbde212f9699abd34609b2a8755
Fabian <draizfabian@gmail.com>
2021-01-27 23:17:40 +0200

    string formatting

--- Advanced_Python/various.py
+++ Advanced_Python/various.py
@@ -1,6 +1,14 @@
+from string import Template
 def main():
     b=bytes([0x41,0x42,0x43,0x44])
     print(b)
     s="This a string"
     print(s)
+    s2=b.decode("utf-8")
+    print(s+s2)
+    b2=s.encode("utf-8")
+    print(b+b2)
+    #utf-32
+    b3=s.encode('utf-32')
+    print(b3)
 main()
