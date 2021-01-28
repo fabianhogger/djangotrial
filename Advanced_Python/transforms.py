8c9580b2092346de5dd30986423253ab75580d7e
Fabian <draizfabian@gmail.com>
2021-01-28 18:00:46 +0200

    transforms

---
+++ Advanced_Python/transforms.py
@@ -0,0 +1,44 @@
+# use transform functions like sorted, filter, map
+
+
+def filterFunc(x):
+    if x%2==0:
+        return False
+    return True
+
+
+def filterFunc2(x):
+    if x.isupper():
+        return False
+    return True
+
+
+
+def squareFunc(x):
+    return x*2
+
+
+def toGrade(x):
+    pass
+
+
+def main():
+    # define some sample sequences to operate on
+    nums = (1, 8, 4, 5, 13, 26, 381, 410, 58, 47)
+    chars = "abcDeFGHiJklmnoP"
+    grades = (81, 89, 94, 78, 61, 66, 99, 74)
+
+    # TODO: use filter to remove items from a list
+    odds=list(filter(filterFunc,nums))
+    print(odds)
+    # TODO: use filter on non-numeric sequence
+    lowers=list(filter(filterFunc2,chars))
+    print(lowers)
+    # TODO: use map to create a new sequence of values
+    squares=list(map(squareFunc,nums))
+    print(squares)
+    # TODO: use sorted and map to change numbers to grades
+
+
+if __name__ == "__main__":
+    main()
